6a6651d63ea9c96ce3565be6125b65ed82da2d8f
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-11 14:21:57 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply)
+
+let random = Math.ceil(Math.random()*10);
+
+console.log(random);
+
+let num3 = 14; 
+let num4 = 10;
+let mod =  num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(20);
+console.log(max)
